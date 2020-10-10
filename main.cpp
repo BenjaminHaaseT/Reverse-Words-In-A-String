0d5f7b954726db0a84ed40a83451322de38f2146
JustBenjami <71096387+JustBenjami@users.noreply.github.com>
2020-10-10 12:38:20 -0700

    Full functionality achieved
    Full functionality achieved with test cases

--- main.cpp
+++ main.cpp
@@ -14,17 +14,30 @@ string reverse_words(string s);
 
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    cout << reverse_words("Hello World!") << endl;
+    cout << reverse_words("Let's take LeetCode contest") << endl;
+
     return 0;
 }
 
 
-string reverse_word(string s) {
+string reverse_words(string s) {
+    int len = s.length();
+    string reversed (len, '0');
+    for (int i {}; i < len; i++) {
+        reversed[i] = s[len - 1 - i];
+    }
+    int r_start = len - 1;
+    int position = reversed.find_last_of(' ', r_start);
+    int s_start = 0;
 
-
-
-
-
-
-
+    while (position != string::npos) {
+        s.replace(s_start, r_start - position, reversed.substr(position + 1, r_start - position));
+        s_start += (r_start - position) + 1;
+        r_start = position - 1;
+        position = reversed.find_last_of(' ', r_start);
+    }
+    s.replace(s_start, r_start, reversed.substr(position + 1, r_start - position));
+    cout << s << endl;
+    return s;
 }
